566f83650142fdcfb682e2973b03a91433319f53
hinch32749 <hinch@mail.ru>
2021-12-21 22:39:35 +0300

    customized css

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import index, by_rubric, BbCreateView
 
+# app_name = 'bboard'
+
 urlpatterns = [
-    path('add/', BbCreateView.as_view(), name='add'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path('index/', index, name='index')
+    re_path(r'^add/$', BbCreateView.as_view(), name='add'),
+    re_path(r'^(?P<rubric_id>[0-9]*)/$', by_rubric, name='by_rubric'),
+    re_path(r'^$', index, name='index')
 ]
 
 
