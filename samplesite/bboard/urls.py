0a6968104b7a8a45d2f1706b3fd1e560de018937
hinch32749 <hinch@mail.ru>
2021-12-22 19:12:00 +0300

    Added new class into views.py and new path "detail". Created bb_detail.html and changed by_rubric.html and index.html

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path
 
-from .views import index, by_rubric, BbCreateView
+from .views import index, BbByRubricView, BbCreateView, BbDetailView
 
 # app_name = 'bboard'
 
 urlpatterns = [
+    re_path('^detail/(?P<pk>[0-9]*)/$', BbDetailView.as_view(), name='detail'),
     re_path(r'^add/$', BbCreateView.as_view(), name='add'),
-    re_path(r'^(?P<rubric_id>[0-9]*)/$', by_rubric, name='by_rubric'),
+    re_path(r'^(?P<rubric_id>[0-9]*)/$', BbByRubricView.as_view(), name='by_rubric'),
     re_path(r'^$', index, name='index')
 ]
 
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,18 +1,17 @@
+
 from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView, DetailView
 
 from .models import Bb, Rubric
 from .forms import BbForm
 
-def index(request):
-    bbs = Bb.objects.all()
-    rubrics = Rubric.objects.all()
-    context = {'bbs': bbs, 'rubrics': rubrics}
-    return render(request, 'bboard/index.html', context)
+# ========================================================================================================
+# Всякие примеры из книги
 
 
+# Потоковый ответ представляется классом StreamingHttpResponse.
 # def index(request):
 #     bbs = Bb.objects.all()
 #     resp = StreamingHttpResponse(bbs,
@@ -20,15 +19,54 @@ def index(request):
 #     return resp
 
 
-def by_rubric(request, rubric_id):
-    bbs = Bb.objects.filter(rubric=rubric_id)
-    rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
-    return render(request, 'bboard/by_rubric.html', context)
+# Для отправки клиентам файлов применяется класс FileResponse
+# Отправка картинки Джафара:)
+# from django.http import FileResponse
+# def index(request):
+#     filename = r'd:/jafar.png'
+#     return FileResponse(open(filename, 'rb'))
+
+
+# Для отправки данных в формате JSON применяется класс JsonResponse
+# from django.http import JsonResponse
+# from django.core.serializers.json import DjangoJSONEncoder
+# def index(request):
+#     data = {'title': 'Bicycle', 'content': 'Old', 'price': 10000.0}
+#     return JsonResponse(data, encoder=DjangoJSONEncoder, safe=False)
+
+# ========================================================================================================
+class BbDetailView(DetailView):
+    """Выводит описание выбранного объявления."""
+    model = Bb
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
+
+
+class BbByRubricView(TemplateView):
+    """Выводит выбранную рубрику и все ее объявления."""
+    template_name = 'bboard/by_rubric.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['bbs'] = Bb.objects.filter(rubric=context['rubric_id'])
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
+        return context
+
+
+# def by_rubric(request, rubric_id):
+#     bbs = Bb.objects.filter(rubric=rubric_id)
+#     rubrics = Rubric.objects.all()
+#     current_rubric = Rubric.objects.get(pk=rubric_id)
+#     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+#     return render(request, 'bboard/by_rubric.html', context)
 
 
 class BbCreateView(CreateView):
+    """Создание нового объявления."""
     template_name = 'bboard/create.html'
     form_class = BbForm
     success_url = reverse_lazy('index')
@@ -37,3 +75,10 @@ class BbCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
         return context
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
